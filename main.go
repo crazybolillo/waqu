2aff0d056d501a8cd9ecfb340244d0d93361b2f1
CrazyBolillo <antonio@zoftko.com>
2024-12-05 21:05:45 -0600

    feat: use pubsub instead of cloud tasks
    It seems like pubsub is better suited for real time messaging. Hopefully
    Im right...

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"bytes"
-	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
-	taskspb "cloud.google.com/go/cloudtasks/apiv2/cloudtaskspb"
+	"cloud.google.com/go/pubsub"
 	"context"
 	"encoding/json"
 	"github.com/caarlos0/env/v11"
@@ -16,12 +15,11 @@ import (
 )
 
 type config struct {
-	Token                   string `env:"TOKEN,required"`
-	QueuePath               string `env:"QUEUE_PATH,required"`
-	HttpTarget              string `env:"HTTP_TARGET,required"`
-	HttpTargetAuthorization string `env:"HTTP_TARGET_AUTHORIZATION,required"`
-	Port                    string `env:"PORT" envDefault:"8080"`
-	IgnoreStatus            bool   `env:"IGNORE_STATUS" envDefault:"true"`
+	ProjectID    string `env:"PROJECT_ID,required"`
+	TopicId      string `env:"TOPIC_ID,required"`
+	Token        string `env:"TOKEN,required"`
+	Port         string `env:"PORT" envDefault:"8080"`
+	IgnoreStatus bool   `env:"IGNORE_STATUS" envDefault:"true"`
 }
 
 func main() {
@@ -36,14 +34,25 @@ func run(ctx context.Context) int {
 		return 1
 	}
 
-	client, err := cloudtasks.NewClient(ctx)
+	client, err := pubsub.NewClient(ctx, cfg.ProjectID)
 	if err != nil {
 		slog.Error("Failed to create client", "reason", err)
 		return 1
 	}
 	defer client.Close()
 
-	http.HandleFunc("POST /waqu", handleRequest(client, cfg))
+	topic := client.Topic(cfg.TopicId)
+	ok, err := topic.Exists(context.Background())
+	if err != nil {
+		slog.Error("Failed to check if topic exists", "reason", err)
+		return 1
+	}
+	if !ok {
+		slog.Error("Topic does not exist", "id", cfg.TopicId)
+		return 1
+	}
+
+	http.HandleFunc("POST /waqu", handleRequest(topic, cfg))
 
 	address := ":" + cfg.Port
 	slog.Info("Starting server", "address", address)
@@ -76,7 +85,7 @@ func isMessageEvent(content io.Reader) (bool, error) {
 	return false, nil
 }
 
-func handleRequest(client *cloudtasks.Client, cfg config) func(http.ResponseWriter, *http.Request) {
+func handleRequest(topic *pubsub.Topic, cfg config) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		auth := r.Header.Get("Authorization")
 		token := strings.TrimPrefix(auth, "Bearer ")
@@ -104,30 +113,16 @@ func handleRequest(client *cloudtasks.Client, cfg config) func(http.ResponseWrit
 			}
 		}
 
-		req := &taskspb.CreateTaskRequest{
-			Parent: cfg.QueuePath,
-			Task: &taskspb.Task{
-				MessageType: &taskspb.Task_HttpRequest{
-					HttpRequest: &taskspb.HttpRequest{
-						HttpMethod: taskspb.HttpMethod_POST,
-						Url:        cfg.HttpTarget,
-						Headers: map[string]string{
-							"Authorization": cfg.HttpTargetAuthorization,
-							"Content-Type":  "application/json",
-						},
-						Body: body,
-					},
-				},
-			},
-		}
-
-		resp, err := client.CreateTask(context.Background(), req)
+		res := topic.Publish(r.Context(), &pubsub.Message{
+			Data: body,
+		})
+		id, err := res.Get(r.Context())
 		if err != nil {
-			slog.Error("Failed to create task", "reason", err)
+			slog.Error("Failed to publish message", "reason", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		slog.Info("Enqueued message", "task", resp.Name[strings.LastIndex(resp.Name, "/")+1:])
+		slog.Info("Enqueued message", "id", id)
 	}
 }
